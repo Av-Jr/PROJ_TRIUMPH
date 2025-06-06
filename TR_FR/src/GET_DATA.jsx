2b197311bd7ad0a64e9e820d477cfcd965205531
Av-Jr <aryansh2005vashishtha@gmail.com>
2025-06-06 22:35:41 +0530

    Update GET_DATA.jsx

--- TR_FR/src/GET_DATA.jsx
+++ TR_FR/src/GET_DATA.jsx
@@ -1,6 +1,6 @@
 function GET_DATA(){
     return(
-        fetch("http://localhost:4000/data")
+        fetch("https://proj-triumph.onrender.com/data")
             .then(response => response.json())
             .then(data => data)
     )
